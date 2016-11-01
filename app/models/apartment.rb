4c1a95a1ad70833fef4df95a8a49895183f845e8
Naveed Shah <naveed@tayybah.com>
2016-11-01 15:47:06 -0400

    Made changes to erb files

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,5 +3,5 @@ class Apartment < ApplicationRecord
   has_many :lists, :through => :apartment_lists
   has_many :tours
   scope :most_popular, -> { joins(:lists).group(:apartment_id).having("COUNT(*)").order('id DESC').limit(3) }
-  scope :lowest_highest, -> {order('price_range ASC')}
+  scope :lowest_highest, -> {order('price ASC')}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-@manhattan = Scraper.new
-@manhattan.insert_index_page_data("manhattan")
+manhattan = Scraper.new
+manhattan.insert_index_page_data("manhattan")
 
-@queens = Scraper.new
- @queens.insert_index_page_data("queens")
+queens = Scraper.new
+queens.insert_index_page_data("queens")
 
- @brooklyn = Scraper.new
- @brooklyn.insert_index_page_data("brooklyn")
+brooklyn = Scraper.new
+brooklyn.insert_index_page_data("brooklyn")
 
- @show_page_scraper = Scraper.new
+@show_page_scraper = Scraper.new
 
  Apartment.all.each do |apartment|
    @show_page_scraper.insert_show_page_data(apartment)
