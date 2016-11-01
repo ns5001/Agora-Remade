28e1adb4b077f4b1b4c9f7d1aca527181a3fa186
Isuru <github email address>
2016-11-01 15:19:22 -0400

    got devise functioning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,20 @@ class ApplicationController < ActionController::Base
     request.env['omniauth.origin'] || root_path
   end
 
-  def index
+  def current_hour
+    time = Time.new
+    time.hour
   end
 
-  
+  def get_greeting
+    if current_hour < 12
+      greeting = "Good Morning"
+    elsif current_hour > 12 && current_hour < 18
+      greeting = "Good Afternoon"
+    else
+      greeting = "Good Evening"
+    end
+    greeting
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+  def show
+    @greeting = get_greeting
+  end
+  
 end
